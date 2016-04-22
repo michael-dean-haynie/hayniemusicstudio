fdf3bcb3895d21cc15823d4205228d29d37856d8
michael-dean-haynie <michael.dean.haynie>
2016-04-22 09:16:33 -0700

    spacing/layout

--- resources/views/pages/private-lessons.blade.php
+++ resources/views/pages/private-lessons.blade.php
@@ -11,23 +11,17 @@ Private Lessons | Haynie Music Studio
 @endsection
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="page-header">
-                <div class='container'>
-                    <div class='row'>
-                        <div class='col-xs-12 col-sm-9'>
-                            <h1>{{$content->find(8)['content']}}
-                                <br>
-                                <small>{{$content->find(9)['content']}}</small>
-                            </h1>
-                        </div>
-                        <div class='col-xs-0 col-sm-3'>
-                            <img class="img-responsive img-rounded" src="{{asset('/img/pianohands.jpg')}}">
-                        </div>
-                    </div>
-                </div>
+<div class="page-header">
+    <div class='container'>
+        <div class='row'>
+            <div class='col-xs-12 col-sm-9'>
+                <h1>{{$content->find(8)['content']}}
+                    <br>
+                    <small>{{$content->find(9)['content']}}</small>
+                </h1>
+            </div>
+            <div class='col-xs-0 col-sm-3'>
+                <img class="img-responsive img-rounded" src="{{asset('/img/pianohands.jpg')}}">
             </div>
         </div>
     </div>
